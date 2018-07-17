c19a1de6c08a8f158152709ea18afbb5a9971abc
exowanderer <jdfraine@gmail.com>
2018-07-17 16:27:22 -0400

    Cleaned up command line tool

--- Phase Curve Exoplanet TSO - Photometric Extraction Pipeline.py
+++ Phase Curve Exoplanet TSO - Photometric Extraction Pipeline.py
@@ -14,6 +14,7 @@ ap.add_argument('-m', '--method', required=False, type=str, default='median', he
 ap.add_argument('-t', '--telescope', required=False, type=str, default='Spitzer', help='Telescope: [Spitzer, Hubble, JWST].')
 ap.add_argument('-ou', '--outputUnits', required=False, type=str, default='electrons', help='Units for the extracted photometry [electrons, muJ_per_Pixel, etc].')
 ap.add_argument('-d', '--data_dir', required=False, type=str, default='', help='Set location of all `bcd` and `bunc` files: bypass previous setup.')
+ap.add_argument('-d', '--verbose', required=False, type=bool, default=False, help='Print out normally irrelevent things.')
 args = vars(ap.parse_args())
 
 planetName = args['planet_name']
@@ -131,8 +132,8 @@ else:
 header_test = fits.getheader(fitsFilenames[0])
 print('\n\nAORLABEL:\t{}\nNum Fits Files:\t{}\nNum Unc Files:\t{}\n\n'.format(header_test['AORLABEL'], len(fitsFilenames), len(uncsFilenames)))
 
-if verbose == 10: print(fitsFilenames)
-if verbose == 10: print(uncsFilenames)
+if verbose: print(fitsFilenames)
+if verbose: print(uncsFilenames)
 
 # Necessary Constants Spitzer
 ppm = 1e6
