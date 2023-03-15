6432f21fc67ad52bd4dc5b032b9371b5532bace5
Jonathan Fraine <jdfraine@gmail.com>
2023-03-15 12:41:24 +0100

    added shell script to run multiple AORs

---
+++ extract_all_example_chX.sh
@@ -0,0 +1,11 @@
+echo "r12345678"
+python example_exoplanet_tso_photometric_extraction_pipeline.py -pn planetname -c chX -ad r12345678  -d aordirs/;
+
+echo "r11235813"
+python example_exoplanet_tso_photometric_extraction_pipeline.py -pn planetname -c chX -ad r11235813  -d aordirs/;
+
+echo "r87654321"
+python example_exoplanet_tso_photometric_extraction_pipeline.py -pn planetname -c chX -ad r87654321  -d aordirs/;
+
+echo "r31853211"
+python example_exoplanet_tso_photometric_extraction_pipeline.py -pn planetname -c chX -ad r31853211  -d aordirs/;
